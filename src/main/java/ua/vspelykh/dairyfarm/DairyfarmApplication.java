eeae2baddcc13e545784ac23d446262e4ed3e134
Vladyslav Pelykh <vspelykh@gmail.com>
2022-05-20 13:24:34 +0300

    Initial commit
    Signed-off-by: Vladyslav Pelykh <vspelykh@gmail.com>

---
+++ src/main/java/ua/vspelykh/dairyfarm/DairyfarmApplication.java
@@ -0,0 +1,13 @@
+package ua.vspelykh.dairyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyfarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DairyfarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/vspelykh/dairyfarm/DairyfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vspelykh.dairyfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyfarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
